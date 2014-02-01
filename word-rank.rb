b524baf28be25300e0de5a75ee79cff2d88126bd
Mike Roberts <magnuscodex@gmail.com>
2014-01-31 17:29:31 -0800

    Reorganize proof of concept code into a scoring class.

--- word-rank.rb
+++ word-rank.rb
@@ -1,63 +1,66 @@
 #!/usr/bin/env ruby
 
-input = $stdin.gets.chomp
-
-def factorial(n)
-  f = 1
-  for i in 1..n
-    f *= i
+class Fixnum
+  def factorial
+    f = 1
+    for i in 1..self
+      f *= i
+    end
+    return f
   end
-  return f
+
+  alias_method :!, :factorial
 end
 
-alph = input.split("").sort
-counts = []
-i = 0
-while i < alph.size
-  k = i + 1
-  char = alph[i]
-  count = 1
-  while char == alph[k]
-    count += 1
-    k += 1
+class WordScore
+  def initialize(word)
+    @word = word
+    alph = @word.split("").sort
+    @counts = []
+    i = 0
+    while i < alph.size
+      k = i + 1
+      char = alph[i]
+      count = 1
+      while char == alph[k]
+        count += 1
+        k += 1
+      end
+      i = k
+      @counts << count
+    end
+    @alph = alph.uniq
   end
-  i = k
-  counts << count
-end
-alph = alph.uniq
 
-puts counts.join(",")
-puts alph.join(",")
-
-possible = factorial(input.size)
-for e in counts
-  next if e == 1
-  possible /= factorial(e)
-end
-
-puts possible
-
-before = 0
-for i in (0...input.size)
-  ind = alph.index(input[i])
-  prior = 0
-  for j in (0...ind)
-    possible = factorial(input.size - i - 1)
-    for k in (0...counts.size)
-      if j == k
-        possible /= factorial(counts[k] - 1)
-      else
-        possible /= factorial(counts[k])
+  def score(word=@word)
+    before = 0
+    for i in (0...word.size)
+      ind = @alph.index(word[i])
+      prior = 0
+      for j in (0...ind)
+        possible = (word.size - i - 1).factorial
+        for k in (0...@counts.size)
+          if j == k
+            possible /= (@counts[k] - 1).factorial
+          else
+            possible /= (@counts[k]).factorial
+          end
+        end
+        prior += possible
+      end
+      before += prior
+      @counts[ind] -= 1
+      if @counts[ind] == 0
+        @counts.delete_at(ind)
+        @alph.delete_at(ind)
       end
     end
-    prior += possible
-  end
-  before += prior
-  counts[ind] -= 1
-  if counts[ind] == 0
-    counts.delete_at(ind)
-    alph.delete_at(ind)
+    
+    return before + 1
   end
 end
 
-puts before + 1
+input = $stdin.gets.chomp
+
+word_score = WordScore.new(input)
+puts word_score.score
